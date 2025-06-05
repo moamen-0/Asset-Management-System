71b82075060726787ff1dc498787d28bba224658
moamen-0 <212103128@o6u.edu.eg>
2025-06-05 04:14:03 +0300

    enhance the ui auth views

--- AssetManagementSystem.PL/Models/RegisterViewModel.cs
+++ AssetManagementSystem.PL/Models/RegisterViewModel.cs
@@ -1,26 +1,29 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-public class RegisterViewModel
+namespace AssetManagementSystem.PL.Models
 {
-	[Required]
-	public string FullName { get; set; }
+	public class RegisterViewModel
+	{
+		[Required]
+		public required string FullName { get; set; }
 
-	[Required]
-	[EmailAddress]
-	public string Email { get; set; }
+		[Required]
+		[EmailAddress]
+		public required string Email { get; set; }
 
-	[Required]
-	[StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-	[DataType(DataType.Password)]
-	public string Password { get; set; }
+		[Required]
+		[StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+		[DataType(DataType.Password)]
+		public required string Password { get; set; }
 
-	[DataType(DataType.Password)]
-	[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-	public string ConfirmPassword { get; set; }
+		[DataType(DataType.Password)]
+		[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+		public required string ConfirmPassword { get; set; }
 
-	[Required]
-	public string NationalId { get; set; }
+		[Required]
+		public required string NationalId { get; set; }
 
-	[Required]
-	public string RecipientFileNumber { get; set; }
+		[Required]
+		public required string RecipientFileNumber { get; set; }
+	}
 }
